38d0657843b7660e0715e3d6e9f47b29f1e1fa15
Ganda <gandaszn@gmail.com>
2023-05-09 15:26:39 +0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Josua Hutapea on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
